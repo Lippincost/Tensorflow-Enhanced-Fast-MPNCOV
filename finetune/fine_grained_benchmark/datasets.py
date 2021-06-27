d2116488ea07de0c8922a953c5e118c9b02c7d8b
Lippincost <117500233+Lippincost@users.noreply.github.com>
2021-06-27 16:21:44 +0000

    add git

--- finetune/fine_grained_benchmark/datasets.py
+++ finetune/fine_grained_benchmark/datasets.py
@@ -1,1 +1,10 @@
-from __future__ import absolute_import, division, print_
+from __future__ import absolute_import, division, print_function, unicode_literals
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+
+import os
+import os.path
+import scipy.io as sio
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
