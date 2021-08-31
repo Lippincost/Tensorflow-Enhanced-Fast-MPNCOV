a7163eacafe2cc7f07b705191d5bf19437bf9f90
Lippincost <117500233+Lippincost@users.noreply.github.com>
2021-08-31 23:02:13 +0000

    i82860_edac: Report CE events properly

--- finetune/fine_grained_benchmark/datasets.py
+++ finetune/fine_grained_benchmark/datasets.py
@@ -71,4 +71,8 @@ class CUB_dataset(object):
             if self.pretrained and self.arch.startswith('vgg'):
                 # RGB==>BGR for VGG16
                 image = image[..., ::-1]
-                mean = [0.406 *
+                mean = [0.406 * 255, 0.456 * 255, 0.485 * 255]
+                std = None
+            else:
+                mean = [0.485 * 255, 0.456 * 255, 0.406 * 255]
+                std = [
