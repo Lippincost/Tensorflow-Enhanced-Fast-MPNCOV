187fc6df091470fc0eab2ece8a3f575c5abc4855
Lippincost <117500233+Lippincost@users.noreply.github.com>
2021-11-14 19:14:29 +0000

    Gallery

--- finetune/fine_grained_benchmark/datasets.py
+++ finetune/fine_grained_benchmark/datasets.py
@@ -239,4 +239,9 @@ class Cars_dataset(object):
         Args:
           is_training: `bool` for whether the input is for training
           data_dir: `str` for the directory of the training and validation data;
-              if 'null' (the literal string 'null') or implic
+              if 'null' (the literal string 'null') or implicitly False
+              then construct a null pipeline, consisting of empty images
+              and blank labels.
+        """
+        super(Cars_dataset, self).__init__()
+  
