802f158e55c24a13bf994a313ab45982ff7930e1
Lippincost <117500233+Lippincost@users.noreply.github.com>
2021-12-30 13:35:19 +0000

    Update reference.md

--- finetune.sh
+++ finetune.sh
@@ -5,4 +5,9 @@ Here you can easily creat a model by selecting
 an arbitray backbone model and global method.
 You can fine-tune it on your own datasets by
 using a pre-trained model.
-Modify the following set
+Modify the following settings as you wish !
+*********************************************
+!
+
+#***************Backbone model****************
+#Our code provides some mainstream architec
