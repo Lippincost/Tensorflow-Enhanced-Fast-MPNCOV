8ba7c4e536f2d4004c7d2bfe9b4c87a7d0ecd2c0
Lippincost <117500233+Lippincost@users.noreply.github.com>
2021-12-30 10:37:49 +0000

    Package changes

--- finetune.sh
+++ finetune.sh
@@ -1,4 +1,8 @@
 set -e
 :<<!
 *****************Instruction*****************
-Here you can easily creat a
+Here you can easily creat a model by selecting
+an arbitray backbone model and global method.
+You can fine-tune it on your own datasets by
+using a pre-trained model.
+Modify the following set
