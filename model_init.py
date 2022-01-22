607625052830a88f7443395b3f9454ffc29f96be
Lippincost <117500233+Lippincost@users.noreply.github.com>
2022-01-22 09:43:18 +0000

    bump year

--- model_init.py
+++ model_init.py
@@ -7,3 +7,8 @@ __all__ = ['Newmodel', 'get_model']
 class Newmodel(Basemodel):
     """replace the image representation method and classifier
 
+       Args:
+       modeltype: model archtecture
+       representation: image representation method
+       num_classes: the number of classes
+ 
