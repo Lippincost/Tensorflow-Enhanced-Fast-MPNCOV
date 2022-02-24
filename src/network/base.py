f47642ccbff35f18ea7f8042df502fc26573f463
Lippincost <117500233+Lippincost@users.noreply.github.com>
2022-02-24 07:58:32 +0000

    revert code

--- src/network/base.py
+++ src/network/base.py
@@ -11,4 +11,9 @@ from .mpncov_vgg16bn import *
 def get_basemodel(modeltype, pretrained=False):
     modeltype = globals()[modeltype]
     if pretrained == False:
-        warnings.warn('You will use model that randomly initia
+        warnings.warn('You will use model that randomly initialized!')
+    return modeltype(pretrained=pretrained)
+
+class Basemodel(tf.keras.Model):
+    """Load backbone model and reconstruct it into three part:
+       1) feature extractor
