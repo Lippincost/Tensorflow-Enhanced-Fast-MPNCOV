6b09ddbc1f3e82323f4f5faa7dbcc58007307819
Lippincost <117500233+Lippincost@users.noreply.github.com>
2022-06-01 22:25:15 +0000

    atualizações

--- src/network/mpncov_vgg16bn.py
+++ src/network/mpncov_vgg16bn.py
@@ -1,2 +1,11 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-import tensorflow
+import tensorflow as tf
+import tensorflow.keras.layers as layers
+from src.representation.MPNCOV import *
+import scipy.io as sio
+
+__all__ = ['mpncov_vgg16bn']
+
+BATCH_NORM_DECAY = 0.9
+BATCH_NORM_EPSILON = 1e-5
+def
