3481443f84377695ae4e4d0449a89766abe1ee6b
Lippincost <117500233+Lippincost@users.noreply.github.com>
2022-06-02 07:23:57 +0000

    Naming fix

--- src/network/mpncov_vgg16bn.py
+++ src/network/mpncov_vgg16bn.py
@@ -8,4 +8,10 @@ __all__ = ['mpncov_vgg16bn']
 
 BATCH_NORM_DECAY = 0.9
 BATCH_NORM_EPSILON = 1e-5
-def
+def batch_norm(init_zero=False, name=None):
+    if init_zero:
+        gamma_initializer = tf.zeros_initializer()
+    else:
+        gamma_initializer = tf.ones_initializer()
+
+ 
