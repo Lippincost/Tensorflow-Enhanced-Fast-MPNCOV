38b55484e4807f9c06d73851147824532c16ca47
Lippincost <117500233+Lippincost@users.noreply.github.com>
2022-12-09 11:55:59 +0000

    Fix setup.py.

--- trainingFromScratch/cifar100/cifar_dataset.py
+++ trainingFromScratch/cifar100/cifar_dataset.py
@@ -1,3 +1,17 @@
 """CIFAR-10 data set.
 
-See http://www.cs.toronto.
+See http://www.cs.toronto.edu/~kriz/cifar.html.
+"""
+import os
+
+import tensorflow as tf
+
+HEIGHT = 32
+WIDTH = 32
+DEPTH = 3
+
+
+class CifarDataSet(object):
+    """Cifar data set."""
+
+    def __init
