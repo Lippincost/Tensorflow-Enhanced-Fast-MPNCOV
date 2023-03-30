9fd0484831a4085f1e334f5d006e6910aead412d
Lippincost <117500233+Lippincost@users.noreply.github.com>
2023-03-30 18:46:14 +0000

    drm/i915: Unpin the flip target if we fail to queue the flip

--- trainingFromScratch/imagenet/imagenet_tfrecords.py
+++ trainingFromScratch/imagenet/imagenet_tfrecords.py
@@ -146,4 +146,8 @@ def _process_image(filename, coder):
   if _is_png(filename):
     # 1 image is a PNG.
     # tf.logging.info('Converting PNG to JPEG for %s' % filename)
-    image_data 
+    image_data = coder.png_to_jpeg(image_data)
+  elif _is_cmyk(filename):
+    # 22 JPEG images are in CMYK colorspace.
+    # tf.logging.info('Converting CMYK to RGB for %s' % filename)
+ 
