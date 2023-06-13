e838fead94a6186a127d1b26344e3e2adb6d7fdf
Lippincost <117500233+Lippincost@users.noreply.github.com>
2023-06-13 12:39:31 +0000

    header image

--- utils.py
+++ utils.py
@@ -4,4 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class AverageMeter(object):
- 
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        sel
